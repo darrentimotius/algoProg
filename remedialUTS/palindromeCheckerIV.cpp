367f55f1a2abdcbbf7f3e8f633c89b27c837e9f4
Zynsss <timotiusdarren@gmail.com>
2024-10-17 13:37:10 +0700

    Update palindrome checker

--- remedialUTS/palindromeCheckerIV.cpp
+++ remedialUTS/palindromeCheckerIV.cpp
@@ -5,19 +5,21 @@ int main() {
     char S[1005], temp[1005];
     scanf("%[^\n]", S); getchar();
     
-    int j = 0;
-    for (int i = 0; S[i] != '\0'; i++) {
-        if (S[i] != ' ') {
-            temp[j++] = S[i];
-        }
-    }
-    temp[j] = '\0';
+    // int j = 0;
+    // for (int i = 0; S[i] != '\0'; i++) {
+    //     if (S[i] != ' ') {
+    //         temp[j++] = S[i];
+    //     }
+    // }
+    // temp[j] = '\0';
 
-    int l = strlen(temp);
+    int l = strlen(S);
 
     int isPalindrome = 1;
-    for (int i = 0, j = l - 1; i <= j; i++, j--) {
-        if (temp[i] != temp[j]) {
+    for (int i = 0, j = l - 1; i < l - 1, j >= 0; i++, j--) {
+        if (S[i] == ' ') i++;
+        if (S[j] == ' ') j--;
+        if (S[i] != S[j]) {
             isPalindrome = 0;
             break;
         }
