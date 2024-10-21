f0d615ee25ca262c7d6fb8b8ef614e783f08ec8a
Zynsss <timotiusdarren@gmail.com>
2024-10-21 15:38:45 +0700

    update prob a and zybooks

--- Bab10/A-position.cpp
+++ Bab10/A-position.cpp
@@ -1,12 +1,28 @@
 #include <stdio.h>
 
 int binarySearch(int A[], int low, int high, int target){
-    if (low > high) return -1;
-    int mid = low + (high - low)/2;
+    if (high >= low) {
+        int mid = low + (high - low) / 2;
+        if (A[mid] == target) {
+            for (int i = low; i <= mid; i++) {
+                if (A[i] == target) return i;
+            }
+        }
+        if (A[mid] > target) return binarySearch(A, low, mid - 1, target);
+        return binarySearch(A, mid + 1, high, target);
+    }
+    return -2;
 
-    if (A[mid] == target) return mid+1;
-    else if (A[mid] < target) return binarySearch(A, mid + 1, high, target);
-    else if (A[mid] > target) return binarySearch(A, low, mid-1, target);
+    // while (low <= high) {
+    //     int mid = low + (high - low) / 2;
+    //     if (A[mid] == target) {
+    //         for (int i = low; i <= mid; i++) {
+    //             if (A[i] == target) return i;
+    //         }
+    //     }
+    //     if (A[mid] < target) low = mid + 1;
+    //     else high = mid - 1;
+    // }
     return -2;
 }
 
@@ -22,6 +38,6 @@ int main() {
     for (int i = 0; i < M; i++) {
         int temp;
         scanf("%d", &temp);
-        printf("%d\n", binarySearch(array, 0, N - 1, temp));
+        printf("%d\n", binarySearch(array, 0, N - 1, temp) + 1);
     }
 }
--- zyBooks.cpp
+++ zyBooks.cpp
@@ -1,25 +1,44 @@
 #include <stdio.h>
-#include <string.h>
 
 int main(void) {
-   char simonPattern[50];
-   char userPattern[50];
-   int userScore;
-   int i;
+   const char SALARY_PROMPT[] = "\nEnter annual salary (0 to exit): ";
+   int annualSalary;
+   int deduction;
+   int totalDeductions;
+   double taxRate;
+   int taxToPay;
 
-   userScore = 0;
+   printf("%s", SALARY_PROMPT);
+   scanf("%d", &annualSalary);
 
-   scanf("%s", simonPattern);
-   scanf("%s", userPattern);
+   while (annualSalary > 0) {
+      // FIXME: Add a while loop to gather deductions. Use the variables
+      // deduction and totalDeduction for deduction handling.
+      // End the inner while loop when a deduction <= 0 is entered.
 
-   /* Your solution goes here  */
-   int l = strlen(simonPattern);
-   for (int i = 0; i < l; i++) {
-    if (simonPattern[i] == userPattern[i]) userScore++;
-    else break;
+      // Determine the tax rate from the annual salary
+      if (annualSalary <= 20000) {
+         taxRate = 0.10;        // 0.10 is 10% written as a decimal
+      }
+      else if (annualSalary <= 50000) {
+         taxRate = 0.20;
+      }
+      else if (annualSalary <= 100000) {
+         taxRate = 0.30;
+      }
+      else {
+         taxRate = 0.40;
+      }
+
+      taxToPay = (int)(annualSalary * taxRate);   // Truncate tax to an integer amount
+      printf("Annual salary: %d\n", annualSalary);
+      printf("Tax rate: %lf\n", taxRate);
+      printf("Tax to pay: %d\n", taxToPay);
+
+      // Get the next annual salary
+      printf("%s\n", SALARY_PROMPT);
+      scanf("%d", &annualSalary);
    }
-
-   printf("userScore: %d\n", userScore);
 
    return 0;
 }
