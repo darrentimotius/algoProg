09f02b797620c162ba86de2c237b310476ddc5dc
Zynsss <timotiusdarren@gmail.com>
2024-10-25 11:23:55 +0700

    Update kelas and template

--- template.cpp
+++ template.cpp
@@ -10,7 +10,7 @@ void swap(int *a, int *b) {
 // bubble sort
 void bubbleSort(int array[], int n) {
     for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
+        for (int j = 0; j < n - 1 - i; j++) {
             if (array[j] > array[j+1]) swap(&array[j], &array[j+1]);
         }
     }
