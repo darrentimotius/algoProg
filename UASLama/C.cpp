b3d970fcd606efd7076747a096c7bf93930a0768
Zynsss <timotiusdarren@gmail.com>
2024-12-06 14:41:54 +0700

    Update problem C

--- UASLama/C.cpp
+++ UASLama/C.cpp
@@ -1,67 +1,43 @@
-#include <stdio.h>
+#include<stdio.h>
 
-char map[105][105];
-void fillMap(int x1, int y1, int x2, int y2, int n) {
-    if (x1 < 1 || n < x1 || y1 < 1 || n < y1) return;
-    if (x1 == x2 && y1 == y2) {
-        map[y1][x1] = '*';
-        return;
+struct pulau{
+    int x1, x2, y1, y2;
+};
+
+struct player{
+    int x, y;
+};
+
+int main(){
+    pulau arr[105];
+    player ppti[105];
+
+    int s, n, d;
+    scanf("%d", &s);
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++) {
+        scanf("%d %d %d %d", &arr[i].x1, &arr[i].y1, &arr[i].x2, &arr[i].y2);
     }
 
-    map[y1][x1] = '*'; // x = kolom, y = baris
+    scanf("%d", &d);
 
-    if (x1 < x2) fillMap(x1 + 1, y1, x2, y2, n); // right
-    else if (x1 > x2) fillMap(x1 - 1, y1, x2, y2, n); // left
-    
-    if (y1 < y2)fillMap(x1, y1 + 1, x2, y2, n); // up
-    else if (y1 > y2) fillMap(x1, y1 - 1, x2, y2, n); // down
-}
+    for(int i = 0; i < d; i++){
+        scanf("%d %d",&ppti[i].x, &ppti[i].y);
+    }
 
-int main() {
-    FILE *f = fopen("testdata.in", "r");
+    int count = 0;
 
-    int n;
-    fscanf(f, "%d\n", &n);
-
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= n; j++) {
-            map[i][j] = '#';
+    for(int i = 0; i < n; i++){
+        int flag = 0;
+        for(int j = 0; j < d; j++){
+            if(arr[i].x1 <= ppti[j].x && ppti[j].x <= arr[i].x2 && arr[i].y1 <= ppti[j].y && ppti[j].y <= arr[i].y2){
+                flag = 1;
+                break;
+            }
         }
+        if(flag == 0) count++;
     }
-
-    int pulau;
-    fscanf(f, "%d\n", &pulau);
-
-    for (int i = 0; i < pulau; i++) {
-        int x1, y1, x2, y2; // x = kolom, y = baris
-        fscanf(f, "%d %d %d %d\n", &x1, &y1, &x2, &y2);
-        fillMap(x1, y1, x2, y2, n);
-        // 2  1  2  2
-        // x1 y1 x2 y2
-    }
-
-    char newMap[105][105];
-    
-
-    for (int i = n; i > 0; i--) {
-        for (int j = 1; j <= n; j++) {
-            printf("%c", map[i][j]);
-        }
-        printf("\n");
-    }
+    printf("%d\n", count);
     return 0;
 }
-
-/*
-10 *****#####
-9  ######*###
-8  #****#*#**
-7  ######*###
-6  ****##*#*#
-5  ######*###
-4  ##*#*#*###
-3  ##*#*##*#*
-2  #*##*##*#*
-1  #*##*##*#*
-   12345678910
-*/
